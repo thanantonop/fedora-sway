71f7f41067ccffa98a1e3d6b7b7cbe0e90c6657d
Thanos Antonopoulos <78595164+thanantonop@users.noreply.github.com>
2024-10-25 14:06:36 +0300

    fedora-sway
    fedora-sway

---
+++ fedora.sh
@@ -0,0 +1,12 @@
+sudo dnf offline-upgrade download -y
+sudo dnf offline-upgrade reboot -y
+
+sudo dnf groupinstall "Development Tools" -y
+sudo dnf install \
+https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf group install Multimedia -y
+
+sudo dnf install python3 python3-pip neofetch ranger nmap emacs vim neovim zip mpv vlc geany meld libreoffice transmission-gtk evince keepassxc chromium tree gimp inkscape darktable krita filezilla  blueman flatpak galculator fontawesome-fonts-all powerline-fonts terminus-fonts liberation-fonts google-roboto-fonts google-roboto-mono-fonts -y
+
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.visualstudio.code -y
