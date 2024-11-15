e11ecb183803f9bf7a92cccd9c3b176e51d9ff97
Thanos Antonopoulos <thanantonop@gmail.com>
2024-11-16 01:27:36 +0200

    fedora-sway

--- fedora.sh
+++ fedora.sh
@@ -6,7 +6,7 @@ https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fed
 sudo dnf4 groupinstall "Development Tools" -y
 sudo dnf4 group install "Multimedia" -y
 
-sudo dnf install python3 python3-pip fastfetch lsd ranger htop btop nmap emacs vim neovim zip mpv vlc geany meld libreoffice transmission-gtk evince keepassxc chromium tree gimp inkscape darktable krita filezilla  blueman flatpak galculator fontawesome-fonts-all powerline-fonts terminus-fonts liberation-fonts google-roboto-fonts google-roboto-mono-fonts -y
+sudo dnf install python3 python3-pip fastfetch lsd fish ranger htop btop nmap emacs vim neovim zip mpv vlc geany meld libreoffice transmission-gtk evince keepassxc chromium tree gimp inkscape darktable krita filezilla blueman flatpak galculator fontawesome-fonts-all powerline-fonts terminus-fonts liberation-fonts google-roboto-fonts google-roboto-mono-fonts -y
 sudo dnf install hyprland hyprland-devel -y
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
