8ec4a4618b60069fb16f087f945c05ae57736574
Thanos Antonopoulos <78595164+thanantonop@users.noreply.github.com>
2024-11-15 19:09:20 +0200

    fedora.sh
    fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,12 +1,13 @@
 sudo dnf offline-upgrade download -y
-sudo dnf offline-upgrade reboot -y
+sudo dnf5 offline reboot -y
 
-sudo dnf groupinstall "Development Tools" -y
 sudo dnf install \
 https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf group install Multimedia -y
+sudo dnf4 groupinstall "Development Tools" -y
+sudo dnf4 group install "Multimedia" -y
 
 sudo dnf install python3 python3-pip neofetch ranger htop btop nmap emacs vim neovim zip mpv vlc geany meld libreoffice transmission-gtk evince keepassxc chromium tree gimp inkscape darktable krita filezilla  blueman flatpak galculator fontawesome-fonts-all powerline-fonts terminus-fonts liberation-fonts google-roboto-fonts google-roboto-mono-fonts -y
+sudo dnf install hyprland hyprland-devel -y
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.visualstudio.code -y
