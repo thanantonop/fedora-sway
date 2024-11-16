18d79ff58663ad09e8266add6ae029449c2ade67
Thanos Antonopoulos <thanantonop@gmail.com>
2024-11-16 11:30:33 +0200

    fedora-sway-hypr

--- fedora.sh
+++ fedora.sh
@@ -6,7 +6,7 @@ https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fed
 sudo dnf4 groupinstall "Development Tools" -y
 sudo dnf4 group install "Multimedia" -y
 
-sudo dnf install python3 python3-pip fastfetch lsd fish ranger htop btop nmap emacs vim neovim zip mpv vlc geany meld libreoffice transmission-gtk evince keepassxc chromium tree gimp inkscape darktable krita filezilla blueman flatpak galculator fontawesome-fonts-all powerline-fonts terminus-fonts liberation-fonts google-roboto-fonts google-roboto-mono-fonts -y
+sudo dnf install python3 python3-pip fastfetch kitty bat lsd fish playerctl brightnessctl wpctl ranger htop btop nmap emacs vim neovim zip mpv vlc geany meld libreoffice transmission-gtk evince keepassxc chromium tree gimp inkscape darktable krita filezilla blueman flatpak galculator fontawesome-fonts-all powerline-fonts terminus-fonts liberation-fonts google-roboto-fonts google-roboto-mono-fonts -y
 sudo dnf install hyprland hyprland-devel -y
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -21,7 +21,7 @@ unzip Meslo.zip -d ~/.fonts
 
 cd ~
 cd ~/Downloads/fedora-sway/
-cp -R sway waybar foot kanshi kitty ~/.config
+cp -R sway waybar foot kanshi hypr kitty ~/.config
 cp -R backgrounds ~/Pictures
 cd ~
 
